1f042818b529d018a617096f19edf0c1451aff5c
soomin9106 <soomin9106@naver.com>
2024-01-23 21:36:40 +0900

    to vercel

--- frontend/app/(route)/person/page.tsx
+++ frontend/app/(route)/person/page.tsx
@@ -51,7 +51,7 @@ const AddPerson = () => {
                         {
                             gradeOptions.map((option) => {
                                 return (
-                                    <Option className="text-black" id={option.value} value={option.value}>{option.label}</Option>
+                                    <Option key={option.value} className="text-black" id={option.value} value={option.value}>{option.label}</Option>
                                 )
                             })
                         }
@@ -69,7 +69,7 @@ const AddPerson = () => {
                         {
                             zoneOptions.map((option) => {
                                 return (
-                                    <Option className="text-black" id={option.value} value={option.value}>{option.label}</Option>
+                                    <Option key={option.value} className="text-black" id={option.value} value={option.value}>{option.label}</Option>
                                 )
                             })
                         }

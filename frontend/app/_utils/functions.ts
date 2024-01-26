9c88a3f81c60e03619daca0b51ba9ec94d170783
soomin9106 <soomin9106@naver.com>
2024-01-26 22:27:54 +0900

    deploy backend 반영

--- frontend/app/_utils/functions.ts
+++ frontend/app/_utils/functions.ts
@@ -5,9 +5,11 @@ export const handleChange = (event: { target: { value: any; }; }, setValue: (arg
     setValue(event.target.value)
 }
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_SERVER
+
 export const getCells = async () => {
     try {
-        const response = await axios.get('http://localhost:8000/cells');
+        const response = await axios.get(`${API_BASE_URL}/cells`);
         return response.data;
     } catch (error) {
         console.error('Error fetching cells:', error);
@@ -17,7 +19,7 @@ export const getCells = async () => {
 
 export const getCellsById = async ({ cell_id }: ChildrenGet) => {
     try {
-        const response = await axios.get(`http://localhost:8000/cell/${cell_id}`);
+        const response = await axios.get(`${API_BASE_URL}/cell/${cell_id}`);
         return response.data;
     } catch (error) {
         console.error('Error fetching cell by ID:', error);
@@ -30,7 +32,7 @@ export const addCell = async ({ name }: CellPost) => {
         const payload = {
             name
         };
-        const response = await axios.post('http://localhost:8000/cell', payload);
+        const response = await axios.post(`${API_BASE_URL}/cell`, payload);
         return response;
     } catch (error) {
         console.error('Error adding cell:', error);
@@ -44,7 +46,7 @@ export const editCell = async ({ id, name }: CellUpdate) => {
             id,
             name
         };
-        const response = await axios.put('http://localhost:8000/cell', payload);
+        const response = await axios.put(`${API_BASE_URL}/cell`, payload);
         return response;
     } catch (error) {
         console.error('Error adding cell:', error);
@@ -54,7 +56,7 @@ export const editCell = async ({ id, name }: CellUpdate) => {
 
 export const getChildren = async ({ cell_id }: ChildrenGet) => {
     try {
-        const response = await axios.get(`http://localhost:8000/children/${cell_id}`);
+        const response = await axios.get(`${API_BASE_URL}/children/${cell_id}`);
         return response.data;
     } catch (error) {
         console.error('Error fetching children:', error);
@@ -70,7 +72,7 @@ export const addChild = async (cell_id: number, props: IStudentInfo) => {
             zone: props.zone,
             talent: Number(props.talent),
         };
-        const response = await axios.post(`http://localhost:8000/child/${cell_id}`, payload);
+        const response = await axios.post(`${API_BASE_URL}/child/${cell_id}`, payload);
         return response;
     } catch (error) {
         console.error('Error adding child:', error);
@@ -86,7 +88,7 @@ export const editChild = async (student_id: number, props: IStudentInfo) => {
             zone: props.zone,
             talent: Number(props.talent),
         };
-        const response = await axios.put(`http://localhost:8000/child/${student_id}`, payload);
+        const response = await axios.put(`${API_BASE_URL}/child/${student_id}`, payload);
         return response;
     } catch (error) {
         console.error('Error adding child:', error);
@@ -96,7 +98,7 @@ export const editChild = async (student_id: number, props: IStudentInfo) => {
 
 export const getChild = async ({ student_id }: ChildGet) => {
     try {
-        const response = await axios.get(`http://localhost:8000/child/${student_id}`);
+        const response = await axios.get(`${API_BASE_URL}/child/${student_id}`);
         if (response.status === 200) {
             return response.data;
         } else {
